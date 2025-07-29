773997eb87f24a38649ff1c6ebcd89c6eec70bc4
sh1ne <max3000.games@gmail.com>
2025-07-29 12:43:36 +0300

    added http logging

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 import { sequelize } from "./db.js";
 import customerRoutes from "./routes/customerRoutes.js";
 import logger from './logger.js';
 
 dotenv.config();
 
-logger.info('Server started successfully on port 3000');
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(morgan('combined', 
+{
+    stream: 
+    {
+        write: (message) => logger.http(message.trim())
+    }
+}));
 
 sequelize.authenticate()
     .then(() => 
@@ -19,8 +26,14 @@ sequelize.authenticate()
         logger.info('Connected to PostgreSQL database');
         return sequelize.sync(); 
     })
-    .catch((err) => { console.error("Error connecting:", err); });
+    .catch((err) => 
+    { 
+        logger.error("Error connecting:", err);
+    });
 
 app.use("/customers", customerRoutes);
 
-app.listen(3000);
+app.listen(3000, () => 
+{
+    logger.info(`Server started successfully on port 3000`);
+});
